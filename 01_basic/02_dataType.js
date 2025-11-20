b17f025e88c27003821eaea917bbd6119fdba4c2
jishan-ah73 <zeeshanahamad225890@gmail.com>
2025-11-20 17:40:31 +0000

    dataType

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,22 @@
+_
+
+let name = "jishan"; // Stirng
+let age = 18;  // Nubmer 
+let isLoggedIn = false;  // boolean
+let state; // undfine
+
+
+// nuber => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null =>standalone vlaue or Empaty vlaue
+// undifined
+// symbole => unique
+
+
+// object
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null);  // object
