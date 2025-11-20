64c51a43824bb9043b98eedf30f484cf33c890d0
jishan-ah73 <zeeshanahamad225890@gmail.com>
2025-11-20 17:40:22 +0000

    variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 12345;
+let accountEmail = "jishan3@gmial.com";
+var accountPassword = "2345";
+accountcity = "lucknow";
+
+
+
+accountEmail = "ahmad2@gmial.com";
+accountPassword = "4566";
+accountcity = "azamgarh";
+
+console.table([accountId, accountEmail,accountPassword,accountcity]);
