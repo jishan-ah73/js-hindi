81086ec2d395d8cba858999ac0f5c1da86249987
jishan-ah73 <zeeshanahamad225890@gmail.com>
2025-11-24 13:23:18 +0000

    Conversion of datatype

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = 0;
+
+//  console.log(typeof score );
+
+// let valueNumber = Number(score);
+// console.log(valueNumber);
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1,  false => 0 
+
+// let isLoggedIn = 1;
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn); 
+
+
+// 1 => true ; 0 =>false
+// "" => false
+// "ahmad" => true
+
+let someNumber = 44;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
