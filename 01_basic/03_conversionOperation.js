ea6ac5648cc7a1fc651be2ab1971658114058f5a
jishan-ah73 <zeeshanahamad225890@gmail.com>
2025-11-25 07:01:35 +0000

    conversion datatype

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -2,15 +2,15 @@ let score = 0;
 
 //  console.log(typeof score );
 
-// let valueNumber = Number(score);
+let valueNumber = Number(score);
 // console.log(valueNumber);
 
 // "33" => 33
 // "33abc" => Nan
 // true => 1,  false => 0 
 
-// let isLoggedIn = 1;
-// let booleanIsLoggedIn = Boolean(isLoggedIn);
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
 // console.log(booleanIsLoggedIn); 
 
 
@@ -20,5 +20,36 @@ let score = 0;
 
 let someNumber = 44;
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//     *************************** Operation  ***************************
+
+let vlaue = 3;
+let newValue = -vlaue;
+// console.log(newValue);
+
+// console.log(2 + 3);
+// console.log(2 - 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+// console.log(2 ** 3);
+
+
+// console.log(1 + 2 + "3");
+// console.log("1"+ 3);
+
+
+
+//   console.log("2" > 1);
+//   console.log(1 >= "0");
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
