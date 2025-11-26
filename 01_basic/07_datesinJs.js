78ac1da39d8e910cd9dcb3bc193026c42ce6afaf
jishan-ah73 <zeeshanahamad225890@gmail.com>
2025-11-26 08:06:49 +0000

    dates

--- 01_basic/07_datesinJs.js
+++ 01_basic/07_datesinJs.js
@@ -1,3 +1,28 @@
 
 let myDate = new Date();
-console.log(myDate);
+// console.log(myDate.toString());
+
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+
+// let myCreatedDate = new Date(2025, 10, 26, 5,3);
+let myCreatedDate = new Date("11-26-2025");
+
+// console.log((myCreatedDate.toLocaleDateString()))
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Date.now());
+
+let newDate = new Date();
+// console.log(newDate.getDate());
+// console.log(newDate.getMonth());
+
+
+
+
+
+
+
