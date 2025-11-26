11e93c2469e6056bb316ed5eb2fa1dc693462033
jishan-ah73 <zeeshanahamad225890@gmail.com>
2025-11-26 07:23:37 +0000

    math and nums

---
+++ 01_basic/06_nums_and_math.js
@@ -0,0 +1,40 @@
+const score = 400;
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(typeof balance.toString());
+
+// const otherNumber = 23;
+// console.log(otherNumber.toFixed(1));
+// console.log(otherNumber.toFixed(3));
+
+//  const otherNumber = 23.55;
+//  console.log(otherNumber.toPrecision(3));
+
+ const otherNumber = 123.55;
+//  console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-In'));
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.1));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+
+console.log(Math.random());
+console.log(Math.random() * 10 +1);
+console.log((Math.random() * 10) + 1 );
+console.log(Math.floor(Math.random() * 10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) +min);
+
+
+
+
