f3f2ba0252fb9f2a119a5037f27624a333c3dc4f
jishan-ah73 <zeeshanahamad225890@gmail.com>
2025-11-26 10:15:42 +0000

    arrays

---
+++ 02_basic/01_arrays.js
@@ -0,0 +1,32 @@
+
+const myArr = [0,1,2,3,4,5,6];
+
+// console.log(myArr);
+
+// myArr.push(7);
+// console.log(myArr);
+// myArr.pop()
+// console.log(myArr);
+
+// myArr.unshift(7);
+// console.log(myArr);
+// myArr.shift();
+// console.log(myArr);
+
+// console.log(myArr.includes(5));     // includes() ka mtalab hota hai array me vlaue present hai ya nhi agar array me value present hai to true aur not present false hoga
+
+// const newArr = myArr.join()
+// console.log(newArr);
+// console.log(typeof newArr);
+
+const myn1 = myArr.slice(1,3);
+console.log(myn1);
+
+const myn2 = myArr.splice(1,3);
+console.log(myn2);
+
+
+
+
+
+
