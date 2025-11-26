fb5fa6338a551e5b8e182c47890af5a46f298e26
jishan-ah73 <zeeshanahamad225890@gmail.com>
2025-11-26 10:37:44 +0000

    arraystwo

---
+++ 02_basic/02_arrays.js
@@ -0,0 +1,31 @@
+
+
+const marvel_hero = ["thor", "spiderman", "ironman"];
+// console.log(marvel_hero);
+
+const dc_heros = ["batman", "superman","flase"];
+
+// marvel_herp.push(dc_heros);
+// console.log(marvel_herp);
+
+//  const allheros = marvel_hero.concat(dc_heros);
+// console.log(allheros);
+
+// const newallheros = [...marvel_hero, ...dc_heros];
+// console.log(newallheros);
+
+// const newArr = [1,2,3,5,[3,5,6],[2,4],[4,8,[4,7,8]]];
+// console.log(newArr.flat(Infinity));
+
+// console.log(Array.isArray("ahmad"));
+// console.log(Array.from("ahmad"));
+// console.log(Array.from({name: "ahamd"}));
+
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+console.log(Array.of(score1, score2, score3));
+
+
+
