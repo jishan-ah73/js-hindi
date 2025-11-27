41f36449b014988727d7523209a1d9bfc3933a55
jishan-ah73 <zeeshanahamad225890@gmail.com>
2025-11-27 07:15:54 +0000

    Arrow function

---
+++ 03_basic/03_Arrow.js
@@ -0,0 +1,48 @@
+// const { use } = require("react");
+
+const user = {
+    username: "joker",
+    price: 199,
+    welcomeMessge: function(){
+        // console.log(`${this.username}, welocome to website`);
+        // console.log(this);
+        
+        
+    }
+}
+// user.welcomeMessge()
+// user.username = "jan"
+// user.welcomeMessge();
+// console.log(this);
+
+
+// function chai(){
+//     let username = "jokerjan";
+//     console.log(this.username);
+    
+// }
+// chai()
+
+// const chai = function(){
+//     let username = "jokerjan";
+//      console.log(this.username);
+// }
+// chai()
+
+
+// const chai = () => {
+//     let username = "jokerjan";
+//      console.log(this);
+// }
+// chai()
+
+
+// const addTwo = (num1, num2) =>{
+//     return num1 + num2;
+// }
+// console.log(addTwo(3,5));
+
+
+// const addTwo = (num1,num2) => num1 + num1
+
+// const addTwo = (num1,num2) => (num1 + num1)
