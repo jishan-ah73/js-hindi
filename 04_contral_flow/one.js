5e6a79c788dacaf097c9f4cd109a4835546077ca
jishan-ah73 <zeeshanahamad225890@gmail.com>
2025-11-27 10:35:22 +0000

    operators

---
+++ 04_contral_flow/one.js
@@ -0,0 +1,22 @@
+
+// const temperatue = 41;
+
+// if(temperatue < 50){
+//     console.log("less than 50");
+    
+// }
+// else{
+//     console.log("temperature is grather than 50");
+    
+// }
+
+
+
+// nullish coalescing Operator (??) null undefined
+
+let value1;
+// valuex1 = 5 ?? 10;
+// value1 = null ?? 10;
+
+value1 = undefined ?? 15;
+console.log(value1);
